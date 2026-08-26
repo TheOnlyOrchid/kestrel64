e75ff771225ea6a60db80ff64981036119f60d7a
Orchid <theonlyorchid@kestrelsystems.dev>
2026-08-27 00:16:57 +0100

    renamed + new file for print_cstr - implementation is just a copy of prINT

---
+++ src/print/print_cstr.asm
@@ -0,0 +1,81 @@
+; src/print/print.asm
+; Example function: prINT
+; Writes a buffer to stdout on Windows x64.
+;
+; ABI (Win64):
+;   RCX = pointer to bytes
+;   RDX = byte length
+; Returns:
+;   RAX = bytes written (or 0 on failure)
+
+bits 64
+default rel
+
+global prINT
+
+extern GetStdHandle
+extern WriteFile
+
+; my functions
+extern GetCstrLength
+extern IntToAscii
+
+section .text
+
+; prINT =================
+;
+; Writes an int to stdout
+;
+; args:
+; rax - the INT to prINT!
+printCstr:
+    ; Shadow space + stack alignment for Win64 calls
+    sub rsp, 64
+
+    ; Save args for WriteFile call
+    mov qword [rsp + 56], rax
+    ; get Cstr to print
+    call IntToAscii ; rax = ptr to cstr
+    mov qword [rsp + 56], rax
+
+    ; get cstr length
+    mov rcx, rax ; rcx = arg1
+    call GetCstrLength
+    mov dword [rsp + 48], eax
+
+    ; HANDLE GetStdHandle(DWORD nStdHandle = -11)
+    ; Returns in RAX
+    mov ecx, -11
+    call GetStdHandle
+
+    ; BOOL WriteFile(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED)
+    ; rcx = handle
+    ; rdx = buffer
+    ; r8d = length (DWORD)
+    ; r9  = &writte
+    ; dword rsp+32 = number of bytes written
+    ; qword lpOverlapped = Null
+    mov rcx, rax
+    mov rdx, qword [rsp + 56]
+    mov r8d, dword [rsp + 48]
+    lea r9, [rsp + 40]
+    mov qword [rsp + 32], 0
+    mov dword [rsp + 40], 0
+    call WriteFile
+
+    ; test result written to EAX != 0
+    test eax, eax
+    jz .fail
+
+    ; set EAX (return value) to the number of bytes written
+    mov eax, dword [rsp + 40]
+    ; restore stack pointer
+    add rsp, 64
+    ret
+
+.fail:
+    ; clear EAX
+    xor eax, eax
+    ; restore stack pointer
+    add rsp, 64
+    ret
